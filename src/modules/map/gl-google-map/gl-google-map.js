8ae89516f54eacc6d9d5043219811409b65f9e92
William Metz <wrmetzjr@gmail.com>
2023-01-18 22:36:39 -0500

    Adding gl-google-map element

---
+++ src/modules/map/gl-google-map/gl-google-map.js
@@ -0,0 +1,63 @@
+/**
+ * @injectHTML
+ */
+export class GlGoogleMap extends HTMLElement {
+    static observedAttributes = ['key'];
+
+    #key = '';
+    #id = crypto.randomUUID ? crypto.randomUUID().split('-').pop() : (Math.random() * 1000);
+    #apiLoadedCBName = `gl_cb_${this.#id}`;
+
+    constructor() {
+        super();
+        this.errors = [];
+        this.utilTimeout = undefined;
+        this.map = undefined;
+        this.elem = this.shadowRoot.querySelector('.map');
+        this.elem.setAttribute('id', `map_${this.#id}`);
+    }
+
+    #handleApiLoaded() {
+        this.map = new google.maps.Map(this.elem, {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    }
+
+    #loadGoogleMapsApi() {
+        const endpoint = 'https://maps.googleapis.com/maps/api/js';
+        const script = document.createElement('script');
+        script.id = `map_script_${this.#id}`;
+        script.type = 'text/javascript';
+        script.src = `${endpoint}?key=${this.#key}&callback=${this.#apiLoadedCBName}&v=weekly`;
+        script.defer = true;
+        script.async = true;
+        window[this.#apiLoadedCBName] = this.#handleApiLoaded.bind(this);
+        document.head.appendChild(script);
+    }
+
+    /**
+     * Fires when the component is connected to the DOM
+     */
+    connectedCallback() {
+
+    }
+
+    /**
+     * Handles changes to the component attributes
+     * @param {String} name
+     * @param {String} oldValue
+     * @param {String} newValue
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'key' && newValue) {
+            this.#key = newValue;
+            this.removeAttribute('key');
+            this.#loadGoogleMapsApi();
+        }
+    }
+}
+
+if (!window.customElements.get('gl-google-map')) {
+    window.customElements.define('gl-google-map', GlGoogleMap);
+}
--- src/web-components.js
+++ src/web-components.js
@@ -9,6 +9,9 @@ export { RadioGroup } from './modules/form/radio-group/radio-group.js';
 // Charts
 export { ChartDonut } from './modules/chart/donut/chart-donut.js';
 
+// Maps
+export { GlGoogleMap } from './modules/map/gl-google-map/gl-google-map.js';
+
 // Complex Components
 export { MortgageCalcInput } from './modules/form/mortgage-calc-input/mortgage-calc-input.js';
 export { MortgageCalc } from './modules/form/mortgage-calc/mortgage-calc.js';
