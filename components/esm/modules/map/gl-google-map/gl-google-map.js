ac24421d35700fde6a96e1364e229e7b0a37a87f
wrmz <wrmz@users.noreply.github.com>
2023-01-19 03:54:08 +0000

    deploy: 2d5438a0b1f7f5bd602a393619d54425a4c6ec15

---
+++ components/esm/modules/map/gl-google-map/gl-google-map.js
@@ -0,0 +1,71 @@
+let google = null;
+
+/**
+ * @injectHTML
+ */
+class GlGoogleMap extends HTMLElement {
+    static get observedAttributes() {
+        return [
+            'key',
+        ];
+    }
+
+    constructor() {
+        super();this.attachShadow({mode:'open'}).innerHTML=`<style>:host{position:relative;display:block;width:100%;min-height:300px}:host .map{position:absolute;top:0;left:0;width:100%;height:100%}</style><div class="map"></div>`;
+        this.errors = [];
+        this.utilTimeout = undefined;
+        this.key = '';
+        this.id = crypto.randomUUID ? crypto.randomUUID().split('-').pop() : (Math.random() * 1000);
+        this.handleApiLoaded = `gl_cb_${this.id}`;
+        this.map = undefined;
+        this.elem = this.shadowRoot.querySelector('.map');
+        this.elem.setAttribute('id', `map_${this.id}`);
+    }
+
+    handleApiLoaded() {
+        this.map = new google.maps.Map(this.elem, {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8
+        });
+    }
+
+    loadGoogleMapsApi() {
+        const endpoint = 'https://maps.googleapis.com/maps/api/js';
+        const script = document.createElement('script');
+        script.id = `map_script_${this.id}`;
+        script.type = 'text/javascript';
+        script.src = `${endpoint}?key=${this.key}&callback=${this.apiLoadedCBName}&v=weekly`;
+        script.defer = true;
+        script.async = true;
+        window[this.apiLoadedCBName] = this.handleApiLoaded.bind(this);
+        document.head.appendChild(script);
+    }
+
+    /**
+     * Fires when the component is connected to the DOM
+     */
+    connectedCallback() {
+
+    }
+
+    /**
+     * Handles changes to the component attributes
+     * @param {String} name
+     * @param {String} oldValue
+     * @param {String} newValue
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'key' && newValue) {
+            this.key = newValue;
+            this.removeAttribute('key');
+            this.loadGoogleMapsApi();
+        }
+    }
+}
+
+if (!window.customElements.get('gl-google-map')) {
+    window.customElements.define('gl-google-map', GlGoogleMap);
+}
+
+export { GlGoogleMap };
+//# sourceMappingURL=gl-google-map.js.map
--- components/web-components.js
+++ components/web-components.js
@@ -421,6 +421,75 @@ var webComponents = (function (exports) {
         window.customElements.define('chart-donut', ChartDonut);
     }
 
+    let google = null;
+
+    /**
+     * @injectHTML
+     */
+    class GlGoogleMap extends HTMLElement {
+        static get observedAttributes() {
+            return [
+                'key',
+            ];
+        }
+
+        constructor() {
+            super();this.attachShadow({mode:'open'}).innerHTML=`<style>:host{position:relative;display:block;width:100%;min-height:300px}:host .map{position:absolute;top:0;left:0;width:100%;height:100%}</style><div class="map"></div>`;
+            this.errors = [];
+            this.utilTimeout = undefined;
+            this.key = '';
+            this.id = crypto.randomUUID ? crypto.randomUUID().split('-').pop() : (Math.random() * 1000);
+            this.handleApiLoaded = `gl_cb_${this.id}`;
+            this.map = undefined;
+            this.elem = this.shadowRoot.querySelector('.map');
+            this.elem.setAttribute('id', `map_${this.id}`);
+        }
+
+        handleApiLoaded() {
+            this.map = new google.maps.Map(this.elem, {
+                center: { lat: -34.397, lng: 150.644 },
+                zoom: 8
+            });
+        }
+
+        loadGoogleMapsApi() {
+            const endpoint = 'https://maps.googleapis.com/maps/api/js';
+            const script = document.createElement('script');
+            script.id = `map_script_${this.id}`;
+            script.type = 'text/javascript';
+            script.src = `${endpoint}?key=${this.key}&callback=${this.apiLoadedCBName}&v=weekly`;
+            script.defer = true;
+            script.async = true;
+            window[this.apiLoadedCBName] = this.handleApiLoaded.bind(this);
+            document.head.appendChild(script);
+        }
+
+        /**
+         * Fires when the component is connected to the DOM
+         */
+        connectedCallback() {
+
+        }
+
+        /**
+         * Handles changes to the component attributes
+         * @param {String} name
+         * @param {String} oldValue
+         * @param {String} newValue
+         */
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === 'key' && newValue) {
+                this.key = newValue;
+                this.removeAttribute('key');
+                this.loadGoogleMapsApi();
+            }
+        }
+    }
+
+    if (!window.customElements.get('gl-google-map')) {
+        window.customElements.define('gl-google-map', GlGoogleMap);
+    }
+
     class MortgageCalcInput extends FieldInput {
         static sanitize(v) { return (v + '').trim().replace(/[^0-9.]*/g, ''); }
 
@@ -730,6 +799,7 @@ var webComponents = (function (exports) {
     exports.ChartDonut = ChartDonut;
     exports.ChevronIcon = ChevronIcon;
     exports.FieldInput = FieldInput;
+    exports.GlGoogleMap = GlGoogleMap;
     exports.InfoMessage = InfoMessage;
     exports.MortgageCalc = MortgageCalc;
     exports.MortgageCalcInput = MortgageCalcInput;
