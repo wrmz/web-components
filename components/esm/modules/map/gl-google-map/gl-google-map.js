c7e0a142c4db50f0214263742ce1c2025c856996
wrmz <wrmz@users.noreply.github.com>
2023-02-08 06:18:21 +0000

    deploy: d2c2f0ee36553ecc1b1559e531786657ff436cc1

--- components/esm/modules/map/gl-google-map/gl-google-map.js
+++ components/esm/modules/map/gl-google-map/gl-google-map.js
@@ -29,14 +29,16 @@ class GlGoogleMap extends HTMLElement {
     }
 
     constructor() {
-        super();const el = document.createElement('template');el.innerHTML = `<style>.lot__content,.lot__image{background:rgb(255 255 255 / 85%);backdrop-filter:brightness(140%) blur(10px)}.lot__content h3 span,.lot__cta{font-weight:300;text-transform:uppercase;color:#fff}:host{position:relative;display:block;width:100%;min-height:410px}:host .gl-map{display:flex;position:absolute;top:0;left:0;width:100%;height:100%;border-radius:4px;overflow:hidden}:host .gl-map__detail,:host .gl-map__map{position:absolute;top:0;height:100%}:host .gl-map__map{left:0;width:100%}:host .gl-map__detail{right:0;width:0;max-width:360px;overflow:hidden;box-sizing:border-box;transition:width .2s ease-out;will-change:width}:host .gl-map__detail::after{content:'';position:absolute;top:0;left:0;width:100%;mix-blend-mode:multiply}:host .gl-map.has-detail .gl-map__detail{width:50%}:host .gl-map__detail-close{cursor:pointer;position:absolute;display:flex;align-items:center;justify-content:center;top:9px;right:9px;width:30px;height:30px;margin:0;padding:0;font-size:30px;font-weight:300;color:#666;border:0;border-radius:4px;background:rgb(255 255 255 / 85%);backdrop-filter:brightness(140%) blur(10px);transition:color .2s ease-out,background .2s ease-out}.lot,.lot__image{position:relative}:host .gl-map__detail-close:focus,:host .gl-map__detail-close:hover{color:#333;background:#fff}:host .gl-map__detail-content{position:relative;top:3px;width:calc(100% - 3px);height:calc(100% - 6px);overflow:auto;box-sizing:border-box}.lot{display:grid;grid-template-rows:max-content 1fr;height:100%}.lot__image{width:100%;height:0;margin-bottom:3px;padding-top:calc(100% * (9 / 16));border-radius:0 4px 0 0}.lot__cta,.lot__img{width:calc(100% - 6px)}.lot__img{position:absolute;display:block;top:3px;left:3px;height:calc(100% - 6px);object-fit:cover;object-position:center;border-radius:2px 4px 2px 2px}.lot__content{position:relative;padding:17px;border-radius:0 0 4px}.lot__content h3{display:grid;grid-auto-flow:column;grid-auto-columns:max-content;gap:3px;margin:0 0 10px}.lot__content h3 span{display:inline-block;margin:0;padding:.25em .75em;font-size:12px;border-radius:2px;background:#333}.lot__content h3 .sold{background:#600}.lot__content h3 .model,.lot__cta{background:#1649c8}.lot__content h3 .pending{background:#9d7f09}.lot__content p{margin:0;font-size:16px}.lot__snapshot{display:grid;grid-template-columns:repeat(3,max-content);gap:.75em;margin:10px 0;line-height:1}.lot__snapshot div:not(:first-child){padding-left:.75em;border-left:1px solid #666}.lot__cta{position:absolute;display:flex;align-items:center;justify-content:center;height:40px;right:3px;bottom:3px;padding:0 17px;font-size:14px;text-decoration:none;border-radius:2px 2px 4px;box-sizing:border-box;transition:background .2s ease-out}.lot__cta:focus,.lot__cta:hover{background:#142755}</style><div class="gl gl-map"><div class="gl-map__map"></div></div>`;this.attachShadow({mode:'open'});this.shadowRoot.appendChild(el.content.cloneNode(true));
+        super();const el = document.createElement('template');el.innerHTML = `<style>.lot__content,.lot__image{background:rgb(255 255 255 / 85%);backdrop-filter:brightness(140%) blur(10px)}.lot__content h3 span,.lot__cta{font-weight:300;text-transform:uppercase;color:#fff}:host{position:relative;display:block;width:100%;min-height:410px}:host .gl-map{display:flex;position:absolute;top:0;left:0;width:100%;height:100%;border-radius:4px;overflow:hidden}:host .gl-map__detail,:host .gl-map__map{position:absolute;top:0;height:100%}:host .gl-map__map{left:0;width:100%}:host .gm-style .gmnoprint.gm-bundled-control{margin:3px!important}:host .gl-map__detail{right:0;width:0;max-width:360px;overflow:hidden;box-sizing:border-box;transition:width .2s ease-out;will-change:width}:host .gl-map__detail::after{content:'';position:absolute;top:0;left:0;width:100%;mix-blend-mode:multiply}:host .gl-map.has-detail .gl-map__detail{width:50%}:host .gl-map__detail-close{cursor:pointer;position:absolute;display:flex;align-items:center;justify-content:center;top:9px;right:9px;width:30px;height:30px;margin:0;padding:0;font-size:30px;font-weight:300;color:#666;border:0;border-radius:4px;background:rgb(255 255 255 / 85%);backdrop-filter:brightness(140%) blur(10px);transition:color .2s ease-out,background .2s ease-out}:host .gl-map__detail-close:focus,:host .gl-map__detail-close:hover{color:#333;background:#fff}:host .gl-map__detail-content{position:relative;top:3px;width:calc(100% - 3px);height:calc(100% - 6px);overflow:auto;box-sizing:border-box}:host .gl-map__legend{position:relative;height:80px;padding:0 33px 0 0;margin:3px;border-radius:3px;box-sizing:border-box;overflow:hidden}:host .gl-map__legend-toggle{cursor:pointer;position:absolute;top:0;right:0;width:80px;height:30px;font-size:20px;letter-spacing:2px;color:#666;border:0;border-radius:3px;box-sizing:border-box;background:rgb(255 255 255 / 85%);backdrop-filter:brightness(140%) blur(10px);transform-origin:top left;transform:rotate(90deg) translate(0,-80px);transition:color .2s ease-out}.lot,.lot__image{position:relative}:host .gl-map__legend-toggle:focus,:host .gl-map__legend-toggle:hover{color:#000}:host .gl-map__legend-drawer{height:100%}:host .gl-map__legend-content{position:relative;display:grid;grid-auto-flow:column dense;grid-auto-columns:minmax(0,1fr);gap:3px;height:100%;padding:3px;box-sizing:border-box;background:rgb(255 255 255 / 65%);backdrop-filter:brightness(140%) blur(6px)}.lot{display:grid;grid-template-rows:max-content 1fr;height:100%}.lot__image{width:100%;height:0;margin-bottom:3px;padding-top:calc(100% * (9 / 16));border-radius:0 4px 0 0}.lot__cta,.lot__img{width:calc(100% - 6px)}.lot__img{position:absolute;display:block;top:3px;left:3px;height:calc(100% - 6px);object-fit:cover;object-position:center;border-radius:2px 4px 2px 2px}.lot__content{position:relative;padding:17px;border-radius:0 0 4px}.lot__content h3{display:grid;grid-auto-flow:column;grid-auto-columns:max-content;gap:3px;margin:0 0 10px}.lot__content h3 span{display:inline-block;margin:0;padding:.25em .75em;font-size:12px;border-radius:2px;background:#333}.lot__content h3 .sold{background:#600}.lot__content h3 .model,.lot__cta{background:#1649c8}.lot__content h3 .pending{background:#9d7f09}.lot__content p{margin:0;font-size:16px}.lot__snapshot{display:grid;grid-template-columns:repeat(3,max-content);gap:.75em;margin:10px 0;line-height:1}.lot__snapshot div:not(:first-child){padding-left:.75em;border-left:1px solid #666}.lot__cta{position:absolute;display:flex;align-items:center;justify-content:center;height:40px;right:3px;bottom:3px;padding:0 17px;font-size:14px;text-decoration:none;border-radius:2px 2px 4px;box-sizing:border-box;transition:background .2s ease-out}.lot__cta:focus,.lot__cta:hover{background:#142755}.legend-item{display:grid;align-items:center;justify-items:center;padding:10px;background:rgb(255 255 255 / 50%);box-sizing:border-box}.legend-item:first-child{border:3px}.legend-item.available svg{color:#666}.legend-item.pending svg{color:#9d7f09}.legend-item.model svg{color:#1649c8}.legend-item.sold svg{color:#600}</style><div class="gl gl-map"><div class="gl-map__map"></div></div>`;this.attachShadow({mode:'open'});this.shadowRoot.appendChild(el.content.cloneNode(true));
 
         registerComponents(GlGoogleMarker);
 
         this.key = '';
         this._id = crypto.randomUUID ? crypto.randomUUID().split('-').pop() : Math.round(Math.random() * 9999);
         this._imageElem = this.querySelector('img');
+        this._legendElem = this.querySelector('gl-google-legend');
         this._markerElems = [...this.querySelectorAll('gl-google-marker')];
+        this._markerPath = 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z';
         this._markers = [];
         this._adminMarkers = [];
         this.apiLoadedCBName = `gl_cb_${this._id}`;
@@ -55,6 +57,7 @@ class GlGoogleMap extends HTMLElement {
 
         this.elem.setAttribute('id', `map_${this._id}`);
 
+        this.generateLegend = this.generateLegend.bind(this);
         this.generateAdminMarker = this.generateAdminMarker.bind(this);
         this.generateMarker = this.generateMarker.bind(this);
         this.loadDetail = this.loadDetail.bind(this);
@@ -168,7 +171,13 @@ class GlGoogleMap extends HTMLElement {
             },
             zoom: 8
         });
+
+
         this.setMapStyle();
+
+        if (this._legendElem) {
+            this.generateLegend();
+        }
 
         // Must create an OverlayView derivitive either way for access
         // to LatLng to pixel methods
@@ -215,10 +224,6 @@ class GlGoogleMap extends HTMLElement {
                 { label: 'sw', latitude: swLatitude, longitude: swLongitude },
             ];
         }
-    }
-
-    generateImage() {
-
     }
 
     generateKml() {
@@ -273,10 +278,12 @@ class GlGoogleMap extends HTMLElement {
             map: this.map,
             id: marker.id,
             type: 'client',
+            status: marker.status,
+            color: marker.color,
             position: { lat: marker.latitude, lng: marker.longitude },
             icon: {
-                path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
-                fillColor: 'red',
+                path: this._markerPath,
+                fillColor: marker.color,
                 fillOpacity: 0.6,
                 strokeWeight: 0,
                 anchor: new google.maps.Point(10, 22)
@@ -284,11 +291,12 @@ class GlGoogleMap extends HTMLElement {
             animation: google.maps.Animation.DROP,
             draggable: this.isAdmin,
         });
+        console.log('marker', marker.status, marker.color, marker.latitude);
         mapMarker.addListener('mouseover', () => {
             if (!mapMarker.isSelected) {
                 mapMarker.setIcon({
-                    path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
-                    fillColor: 'red',
+                    path: this._markerPath,
+                    fillColor: mapMarker.color,
                     fillOpacity: 1,
                     strokeWeight: 0,
                     anchor: new google.maps.Point(10, 22)
@@ -298,8 +306,8 @@ class GlGoogleMap extends HTMLElement {
         mapMarker.addListener('mouseout', () => {
             if (!mapMarker.isSelected) {
                 mapMarker.setIcon({
-                    path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
-                    fillColor: 'red',
+                    path: this._markerPath,
+                    fillColor: mapMarker.color,
                     fillOpacity: 0.6,
                     strokeWeight: 0,
                     anchor: new google.maps.Point(10, 22)
@@ -320,6 +328,14 @@ class GlGoogleMap extends HTMLElement {
             this.dispatchEvent(dragendEvent);
         });
 
+        mapMarker.addListener('mouseover', () => {
+
+        });
+
+        mapMarker.addListener('mouseout', () => {
+
+        });
+
         mapMarker.addListener('click', () => {
             const projection = this.imageLayer.getProjection();
             const pixelPosition = projection.fromLatLngToDivPixel(mapMarker.getPosition());
@@ -332,7 +348,7 @@ class GlGoogleMap extends HTMLElement {
                     if (marker.id === mapMarker.id) {
                         marker.isSelected = true;
                         marker.setIcon({
-                            path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
+                            path: this._markerPath,
                             fillColor: 'white',
                             fillOpacity: 1,
                             strokeWeight: 0,
@@ -341,8 +357,8 @@ class GlGoogleMap extends HTMLElement {
                     } else {
                         marker.isSelected = false;
                         marker.setIcon({
-                            path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
-                            fillColor: 'red',
+                            path: this._markerPath,
+                            fillColor: marker.color,
                             fillOpacity: 0.6,
                             strokeWeight: 0,
                             anchor: new google.maps.Point(10, 22)
@@ -356,6 +372,37 @@ class GlGoogleMap extends HTMLElement {
         });
 
         return mapMarker;
+    }
+
+    generateLegend() {
+        const legendElem = document.createElement('div');
+        const legendToggleElem = document.createElement('button');
+        const legendDrawerElem = document.createElement('div');
+        const legendContentElem = document.createElement('div');
+        const legendContentElems = [...this._legendElem.childNodes];
+
+        legendContentElem.className = 'gl-map__legend-content';
+        legendContentElems.forEach((elem) => {
+            legendContentElem.appendChild(elem);
+        });
+
+        legendDrawerElem.setAttribute('id', `gl_legend_${this._id}`);
+        legendDrawerElem.className = 'gl-map__legend-drawer';
+        legendDrawerElem.appendChild(legendContentElem);
+
+        legendToggleElem.setAttribute('type', 'button');
+        legendToggleElem.setAttribute('id', `gl_legend_toggle_${this._id}`);
+        legendToggleElem.setAttribute('aria-controls', `gl_legend_${this._id}`);
+        legendToggleElem.setAttribute('aria-expanded', false);
+        legendToggleElem.setAttribute('title', 'Show Map Legend');
+        legendToggleElem.className = 'gl-map__legend-toggle';
+        legendToggleElem.textContent = '•••';
+
+        legendElem.className = 'gl-map__legend';
+        legendElem.appendChild(legendDrawerElem);
+        legendElem.appendChild(legendToggleElem);
+
+        this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(legendElem);
     }
 
     generateDetail() {
@@ -409,8 +456,8 @@ class GlGoogleMap extends HTMLElement {
         this.markers.forEach((marker) => {
             if (marker.isSelected && marker.type !== 'admin') {
                 marker.setIcon({
-                    path: 'M10 0c5.52285 0 10 4.47715 10 10 0 7.50794-5.59957 12.48988-10 12.48988S0 17.78101 0 10C0 4.47715 4.47715 0 10 0Zm0 3.4743c-3.60404 0-6.5257 2.92166-6.5257 6.5257 0 3.60404 2.92166 6.5257 6.5257 6.5257 3.60404 0 6.5257-2.92166 6.5257-6.5257 0-3.60404-2.92166-6.5257-6.5257-6.5257Zm0 3.0039c1.94504 0 3.5218 1.57676 3.5218 3.5218 0 1.94504-1.57676 3.5218-3.5218 3.5218-1.94504 0-3.5218-1.57676-3.5218-3.5218 0-1.94504 1.57676-3.5218 3.5218-3.5218Z',
-                    fillColor: 'red',
+                    path: this._markerPath,
+                    fillColor: marker.color,
                     fillOpacity: 0.6,
                     strokeWeight: 0,
                     anchor: new google.maps.Point(10, 22)
